d5aa514e91156d935b4b4a8e3d752ef78c0b7445
Featherwhisker <119465870+WindClan@users.noreply.github.com>
2024-07-28 22:06:32 -0400

    Update example.lua

--- example.lua
+++ example.lua
@@ -2,3 +2,6 @@ term.setBackgroundColor(colors.white)
 term.clear()
 term.setTextColor(colors.black)
 bigfont.writeOn(term.current(), 1, "Server shutdown on 9/27/24", 5, 3)
+while true do
+  sleep()
+end
